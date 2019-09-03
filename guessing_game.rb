279d08fa1779b1a6028b556e7a03e2cfec615596
amatyaraj11 <amatyaraj@gmail.com>
2019-09-03 22:29:22 +1000

    Used sample, shuffle and split to shuffle the word

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,4 +1,4 @@
 puts "Please Guess a word"
 actual_word = ["Apple", "Cat", "Hat", "Boat", "Coat"]
-pick_word = actual_word.split
-p pick_word
+pick_word = actual_word.sample
+p pick_word.split("").shuffle.join(" ")
