b4166a760a134e2462e9ab73f55cdaba565741ca
amatyaraj11 <amatyaraj@gmail.com>
2019-09-04 09:30:18 +1000

    Updated while and if else statement

--- guessing_game.rb
+++ guessing_game.rb
@@ -3,3 +3,12 @@ actual_word = ["Apple", "Cat", "Hat", "Boat", "Coat"]
 pick_word = actual_word.sample
 p pick_word.split("").shuffle.join(" ")
 guess_a_word = gets.chomp.downcase  
+
+
+while guess_a_word != "quit"
+
+if guess_a_word == pick_word.downcase
+    sleep(1)
+    puts "You guessed it right. You win Hippieeee!!!"
+
+end
