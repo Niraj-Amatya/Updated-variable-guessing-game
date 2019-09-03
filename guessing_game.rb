10ed723ae0af5351505de28a69c349912c05589f
amatyaraj11 <amatyaraj@gmail.com>
2019-09-04 08:38:37 +1000

    Updating the downcase

--- guessing_game.rb
+++ guessing_game.rb
@@ -2,3 +2,4 @@ puts "Please Guess a word"
 actual_word = ["Apple", "Cat", "Hat", "Boat", "Coat"]
 pick_word = actual_word.sample
 p pick_word.split("").shuffle.join(" ")
+guess_a_word = gets.chomp.downcase  
