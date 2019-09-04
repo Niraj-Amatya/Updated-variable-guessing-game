aed8aa5ba02fa22f90766696a38d741ca55f3caa
amatyaraj11 <amatyaraj@gmail.com>
2019-09-04 10:11:11 +1000

    added sleep method, making the output slow

--- guessing_game.rb
+++ guessing_game.rb
@@ -11,4 +11,9 @@ if guess_a_word == pick_word.downcase
     sleep(1)
     puts "You guessed it right. You win Hippieeee!!!"
 
+elsif guess_a_word != pick_word.downcase
+    sleep(1)
+    puts "Wrong Guess! Please try again"
+    guess_a_word = gets.chomp.downcase
+
 end
